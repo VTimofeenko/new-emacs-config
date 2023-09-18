4019f030f06e7c72b2d79b85a164da03142f0f87
Vladimir Timofeenko <id@vtimofeenko.com>
2023-09-17 22:16:06 -0700

    feat: global relative line number mode

--- emacs-dir/init.el
+++ emacs-dir/init.el
@@ -1,4 +1,8 @@
 (load-theme 'modus-vivendi-tinted :no-confirm)
+(use-package emacs
+             ;:config
+             (global-display-line-numbers-mode 1)
+             (setq display-line-numbers-type 'visual))
 (use-package evil
              :config
              (evil-mode 1)
