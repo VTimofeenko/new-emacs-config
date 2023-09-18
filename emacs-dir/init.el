4b334cbaa0dcd7326c0ec1b503fa004721bfb4b4
Vladimir Timofeenko <id@vtimofeenko.com>
2023-09-17 22:16:19 -0700

    feat: disable blinking cursor

--- emacs-dir/init.el
+++ emacs-dir/init.el
@@ -1,6 +1,7 @@
 (load-theme 'modus-vivendi-tinted :no-confirm)
 (use-package emacs
              ;:config
+             (blink-cursor-mode 0)
              (global-display-line-numbers-mode 1)
              (setq display-line-numbers-type 'visual))
 (use-package evil
