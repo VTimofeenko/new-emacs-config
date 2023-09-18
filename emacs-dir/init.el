552ae5782b782b60d71b0982cf9f268449f19002
Vladimir Timofeenko <id@vtimofeenko.com>
2023-09-17 22:16:34 -0700

    feat: disable startup screen

--- emacs-dir/init.el
+++ emacs-dir/init.el
@@ -1,5 +1,7 @@
 (load-theme 'modus-vivendi-tinted :no-confirm)
 (use-package emacs
+             :init
+             (setq inhibit-startup-screen t)
              ;:config
              (blink-cursor-mode 0)
              (global-display-line-numbers-mode 1)
