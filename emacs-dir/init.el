369f2607ae757fa9facd9b5da86c930c730713ca
Vladimir Timofeenko <id@vtimofeenko.com>
2023-10-07 18:26:58 -0700

    feat: add evil mode mappings for everything

--- emacs-dir/init.el
+++ emacs-dir/init.el
@@ -18,10 +18,71 @@
              (setq display-line-numbers-type 'visual)
              (setq ring-bell-function 'ignore))
 
+(mapc #'define-prefix-command
+      '(evil-application-map
+         evil-buffer-map
+         evil-error-map
+         evil-file-map
+         evil-git-map
+         evil-global-leader-map
+         evil-helpful-map
+         evil-insert-map
+         evil-jump-map
+         evil-option-map
+         evil-search-map
+         evil-tab-map))
+
 (use-package evil
+             :defer nil
              :init
              (setq evil-want-keybinding nil)
              (setq evil-want-C-u-scroll t)
+             :bind
+             ( :map evil-motion-state-map
+                    ("SPC" . evil-global-leader-map)
+                    ("M-SPC" . evil-global-leader-map)
+                    :map evil-insert-state-map
+                    ("M-SPC" . evil-global-leader-map)
+                    :map evil-global-leader-map
+                    ("a" . evil-application-map)
+                    ("b" . evil-buffer-map)
+                    ("f" . evil-file-map)
+                    ("i" . evil-insert-map)
+                    ("j" . evil-jump-map)
+                    ("o" . evil-option-map)
+                    ("s" . evil-search-map)
+                    ("u" . universal-argument)
+                    ("h" . evil-helpful-map)
+                    ("w" . evil-window-map)
+                    :map evil-window-map
+                    ("d" . evil-window-delete)
+                    :map evil-buffer-map
+                    ; ("b" . list-buffers)
+                    ("B" . bury-buffer)
+                    ("d" . kill-this-buffer)
+                    ("e" . eval-buffer)
+                    ("m" . view-echo-area-messages)
+                    :map evil-file-map
+                    ("a" . write-file)
+                    ("c" . copy-file)
+                    ; ("f" . find-file) ;; Defined in helm so that helm is picked up
+                    ("r" . recover-this-file)
+                    ("s" . save-buffer)
+                    :map evil-jump-map
+                    ("f" . find-function)
+                    ("v" . find-variable)
+                    :map evil-option-map
+                    ("f" . display-fill-column-indicator-mode)
+                    ("s" . window-toggle-side-windows)
+                    ("l" . toggle-truncate-lines)
+                    ("n" . display-line-numbers-mode)
+                    ("d" . toggle-debug-on-error)
+                    ("D" . toggle-debug-on-quit)
+                    :map evil-replace-state-map
+                    ("M-SPC" . evil-global-leader-map)
+                    :map evil-search-map
+                    ("c" . evil-ex-nohighlight)
+                    ("i" . imenu-list-smart-toggle) )
              :config
              (evil-mode 1)
              ;; Leader
@@ -35,17 +96,21 @@
 
 (use-package helpful
              :after evil
+             :bind (:map evil-helpful-map
+                         ("f" . helpful-callable)
+                         ("v" . helpful-variable)
+                         ("c" . helpful-command)
+                         )
              :config
-             (defun mkMapping(key callable)
-               (evil-define-key 'normal 'global (kbd (concat "<leader>h" key)) callable))
-             (mkMapping "f" 'helpful-callable)
-             (mkMapping "v" 'helpful-variable)
-             (mkMapping "c" 'helpful-command))
+             )
 
 (use-package evil-collection
              :after evil
+             :custom (evil-collection-key-blacklist '("SPC"))
              :config
-             (evil-collection-init))
+             (evil-collection-init)
+             (evil-collection-buff-menu-setup)
+             (evil-collection-unimpaired-setup))
 
 (use-package projectile
              :init
@@ -64,6 +129,11 @@
 
 (use-package helm
              :init
+             :bind (:map evil-buffer-map
+                         ("b" . switch-to-buffer)
+                         :map evil-file-map
+                         ("f" . find-file))
+             :config
              (helm-mode 1))
 
 (use-package helm-projectile
